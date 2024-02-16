3f2af0d3997dc49715b14b4451fc89c9081a5d6c
Keyla Soriano <44504724+ksoriano55@users.noreply.github.com>
2024-02-15 22:21:00 -0600

    SQLite
    - Se agrega modelo de contactos
    - Se agrega funcionalidad para creacion de base de datos clase de conexion con BD

---
+++ app/src/main/java/Configuracion/Transacciones.java
@@ -0,0 +1,21 @@
+package Configuracion;
+
+public class Transacciones {
+    public static final String DBName = "PM1E13454";
+
+    public static final String TableContactos = "contactos";
+
+    public static final String id = "id";
+    public static final String nombre = "nombre";
+    public static final String telefono = "telefono";
+    public static final String pais = "pais";
+    public static final  String nota = "nota";
+    public static final String imagen = "imagen";
+
+    public static final String CreateTableContactos = "Create table "+ TableContactos +" ("+
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, telefono INTEGER, pais TEXT, "+
+            "nota TEXT, imagen TEXT )";
+
+    public static final String DropTableContactos = "DROP TABLE IF EXISTS "+ TableContactos;
+
+}
---
+++ app/src/main/java/Models/Contactos.java
@@ -0,0 +1,70 @@
+package Models;
+
+public class Contactos {
+    private Integer Id;
+    private String Pais;
+    private Integer Nombre;
+    private String Telefono;
+    private String Nota;
+    private String Imagen;
+
+    public Contactos(Integer id, String pais, Integer nombre, String telefono, String nota, String imagen) {
+        Id = id;
+        Pais = pais;
+        Nombre = nombre;
+        Telefono = telefono;
+        Nota = nota;
+        Imagen = imagen;
+    }
+
+    public Contactos() {
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String pais) {
+        Pais = pais;
+    }
+
+    public Integer getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(Integer nombre) {
+        Nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+
+    public String getNota() {
+        return Nota;
+    }
+
+    public void setNota(String nota) {
+        Nota = nota;
+    }
+
+    public String getImagen() {
+        return Imagen;
+    }
+
+    public void setImagen(String imagen) {
+        Imagen = imagen;
+    }
+}
