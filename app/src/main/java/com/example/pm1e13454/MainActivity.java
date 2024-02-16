170e6737a0c8b2d0cafb1c448059592a926c9b30
Keyla Soriano <44504724+ksoriano55@users.noreply.github.com>
2024-02-15 20:42:18 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/pm1e13454/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pm1e13454;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
