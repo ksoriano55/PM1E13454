3c321e75da92f0da2c5bdaf8f872fd47c6d15fa4
jAguilar2000 <52142265+jAguilar2000@users.noreply.github.com>
2024-02-16 18:48:07 -0600

    Adicion de pantallas

--- app/src/main/java/com/example/pm1e13454/ActivityList.java
+++ app/src/main/java/com/example/pm1e13454/ActivityList.java
@@ -7,20 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btnListaContacto;
+public class ActivityList extends AppCompatActivity {
+    Button btnRegresarHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list);
 
-        btnListaContacto = (Button) findViewById(R.id.btnListaContacto);
+        btnRegresarHome = (Button) findViewById(R.id.btnRegresarHome);
 
-        btnListaContacto.setOnClickListener(new View.OnClickListener() {
+        btnRegresarHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ActivityList.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
